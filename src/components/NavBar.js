f45ea07ba31fb7cf2880371c92eafa6af83ffae3
christophuff <chris.huff@hotmail.com>
2025-05-19 21:20:03 -0500

    navbar and basic styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,30 +1,54 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
-import { Navbar, Container, Nav, Button } from 'react-bootstrap';
-import { signOut } from '../utils/auth';
+import Image from 'next/image';
+import ProfileDropdown from './ProfileDropdown';
 
 export default function NavBar() {
-  return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Container>
-        <Link passHref href="/" className='navbar-brand'>
-         CHANGE ME
-        </Link>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            {/* CLOSE NAVBAR ON LINK SELECTION: https://stackoverflow.com/questions/72813635/collapse-on-select-react-bootstrap-navbar-with-nextjs-not-working */}
-            <Link className="nav-link" href="/">
-              Home
-            </Link>
-          </Nav>
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-          <Button variant="danger" onClick={signOut}>
-              Sign Out
-          </Button>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter' || e.key === ' ') {
+      toggleMenu(); // Toggle the menu when the Enter or Space key is pressed
+    }
+  };
+
+  return (
+    <nav className="nav-container">
+      <div className="navbar-links">
+        <h1 className="logo">
+          <Link passHref href="/" className="navbar-brand">
+            <Image src="/images/grind-find.png" height="60" width="50" />
+            <span className="logo-brand">GRINDFIND</span>
+          </Link>
+        </h1>
+        <ul className={`nav-links ${isMenuOpen ? 'active' : ''}`}>
+          <li>
+            <Link className="nav-link" href="/">
+              Nearby Parks
+            </Link>
+          </li>
+          <li>
+            <Link className="nav-link" href="/my-uploads">
+              My Uploads
+            </Link>
+          </li>
+          {/* ProfileDropdown aligned to the right */}
+          <li className="profile-icon">
+            <ProfileDropdown />
+          </li>
+        </ul>
+      </div>
+
+      {/* Hamburger Icon with role and keyboard support */}
+      <div className="hamburger" onClick={toggleMenu} onKeyDown={handleKeyDown} role="button" tabIndex={0} aria-label="Toggle navigation menu">
+        <div className={`line ${isMenuOpen ? 'open' : ''}`} />
+        <div className={`line ${isMenuOpen ? 'open' : ''}`} />
+        <div className={`line ${isMenuOpen ? 'open' : ''}`} />
+      </div>
+    </nav>
   );
 }
